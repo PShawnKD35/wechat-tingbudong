67c6ef6920e329767ad71354352b76b422a2a529
OliviaShi <yas089@ucsd.edu>
2019-08-06 16:44:12 +0800

    for first submission

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,100 +1,66 @@
-const app = getApp()
-
+// pages/show/show.js
 Page({
+
+  /**
+   * Page initial data
+   */
   data: {
-// isHide is the user home page
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    isHide: false,
-    DotStyle: true,
+
   },
 
-  onLoad: function () {
-    var that = this;
-// check authorisation granted or not
-    wx.getSetting({
-      success: function (res) {
-        if (res.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            success: function (res) {
-// login check to get openID
-              wx.login({
-                success: res => {
-                  console.log("用户的code:" + res.code);
-                  wx.request({
-                      url: `${app.globalData.url}` + 'login',
-                      method: 'POST',
-                      data: {
-                        code: res.code
-                      },
-                      success: res => {
-                        console.log("???")
-                        console.log(res)
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
 
-
-                      }
-                  });
-                }
-              });
-            }
-          });
-        } else {
-          that.setData({
-            isHide: true
-          });
-        }
-      }
-    });
   },
 
-// get: index page @slangs
-  // onShow: function () {
-  //   let page = this
-  //   wx.request({
-  //     url: `${app.globalData.url}` + 'slangs',
-  //     get: 'GET',
-  //     success: res => {
-  //       console.log(res)
-  //     }
-  //   })
-  // },
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
 
+  },
 
-  bindGetUserInfo: function (e) {
-    if (e.detail.userInfo) {
-      var page = this;
-      console.log("userinfo：");
-      console.log(e.detail.userInfo);
-      //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
-      page.setData({
-        isHide: false
-      });
-      app.globalData.userInfo = e.detail.userInfo;
-      console.log(app.globalData.userInfo);
-      // wx.request({
-      //   url: `${app.globalData.url}users/${userId}`,
-      //   method: 'PUT',
-      //   data: app.globalData.userInfo
-      // })
-    } else {
-      //用户按了拒绝按钮
-      wx.showModal({
-        title: 'Warning',
-        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!',
-        showCancel: false,
-        confirmText: '返回授权',
-        success: function (res) {
-          // 用户没有授权成功，不需要改变 isHide 的值
-          if (res.confirm) {
-            console.log('用户点击了“返回授权”');
-          }
-        }
-      });
-    }
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
   },
-  // cardSwiper
-  cardSwiper(e) {
-    this.setData({
-      cardCur: e.detail.current
-    })
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
   },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
 })

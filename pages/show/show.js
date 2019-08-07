3743309e52eee83e887bdab4c9b514c378c87a78
Irene Chan <irene.chan.16@outlook.com>
2019-08-07 14:39:13 +0800

    looooooooooping

--- pages/show/show.js
+++ pages/show/show.js
@@ -12,55 +12,13 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+    let page = this
+    wx.request({
+      url: `${app.globalData.url}slangs/${option.id}`,
+      method: 'GET',
+      success(res) {
+        console.log(res)
+      }
+    })
   }
 })

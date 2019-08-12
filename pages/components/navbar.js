468efd40d2db0b8927b0f499a66e5fcbf80062e5
OliviaShi <yas089@ucsd.edu>
2019-08-12 19:06:06 +0800

    exploring the component

---
+++ pages/components/navbar.js
@@ -0,0 +1,23 @@
+// pages/components/navbar.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})

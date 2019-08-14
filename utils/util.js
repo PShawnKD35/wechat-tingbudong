72904e2de6dbddfe41714399e08a2d134df127b1
Irene Chan <irene.chan.16@outlook.com>
2019-08-14 09:12:11 +0800

    done with basic set up for favourite stack

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,5 @@
+const app = getApp()
+const AV = require('av-weapp-min.js');
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -16,4 +18,57 @@ const formatNumber = n => {
 
 module.exports = {
   formatTime: formatTime
+  // uploadPromise: uploadPromise
 }
+
+const uploadPromise = imgList => {
+  return new Promise ((resolve, reject)=>{
+      console.log("Starting upload for sticker")
+      let sticker_url = []
+      console.log(imgList)
+      imgList.forEach(function (img) {
+        new AV.File('file-name', {
+          blob: {
+            uri: img,
+          },
+        }).save().then(
+          file => {
+            sticker_url.push(file.url())
+            console.log("this is sticker_url: " + sticker_url)
+            console.log('successfully uploaded stickers')
+          }
+        ).catch(console.error);
+      })
+      // console.log(sticker_url)
+      // this.setData({
+      //   sticker_url: sticker_url
+      // })
+      // let sticker_url = oldsticker_url
+      resolve(sticker_url)
+      console.log("Ending upload for sticker")
+  })
+}
+
+// module.exports = { uploadPromise: uploadPromise}
+
+const apiCall = function (route, methodChoosen, datahash){
+  return new Promise(function (resolve, reject) {
+    wx.request({
+      url: `${app.globalData.url}route`,
+      method: methodChoosen,
+      header: app.globalData.header,
+      data: dataHash, 
+      success: function (res) {
+        resolve(res)
+      }, 
+      fail: function (res) {
+        reject(res)
+      }
+      })
+    }
+  )}
+
+module.exports = { 
+  apiCall: apiCall,
+  uploadPromise: uploadPromise 
+  }
